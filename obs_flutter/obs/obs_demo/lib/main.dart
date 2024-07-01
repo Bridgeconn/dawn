1843a74b9d32162ec27c6b9cf4671e0d7f315dc0
bobbykumar706584 <bobbykumar706584@gmail.com>
2024-07-01 14:17:55 +0530

    audio recorder

--- obs_flutter/obs/obs_demo/lib/main.dart
+++ obs_flutter/obs/obs_demo/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:obs_demo/CreateUserPage.dart';
 import 'package:obs_demo/screen/bottomNavi.dart';
+import 'package:obs_demo/screen/dashboard.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,14 +13,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: CreateUserPage() // Navigate to UserProfilePage directly
-        // home: BottomNavigationBarExample(),
-        );
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      // home: CreateUserPage() // Navigate to UserProfilePage directly
+      home: Dashboard(),
+    );
   }
 }
